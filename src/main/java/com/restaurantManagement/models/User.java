e9352ab13d8a23729f4564ba21ae78468f912ef0
Morteza363831 <120910275+Morteza363831@users.noreply.github.com>
2024-05-22 19:40:11 +0330

    user model created

---
+++ src/main/java/com/restaurantManagement/models/User.java
@@ -0,0 +1,33 @@
+package com.restaurantManagement.models;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NonNull;
+
+@Data
+@EqualsAndHashCode(of = "userId")
+public class User {
+
+    // constants
+    @NonNull
+    private String userId;
+    @NonNull
+    private String fname;
+    @NonNull
+    private String lname;
+    @NonNull
+    private String password;
+    @NonNull
+    private String email;
+    @NonNull
+    private String phone;
+
+    // constructors
+    public User(String fname, String lname, String password, String email, String phone) {
+        this.fname = fname;
+        this.lname = lname;
+        this.password = password;
+        this.email = email;
+        this.phone = phone;
+    }
+}
---
+++ src/main/java/com/restaurantManagement/models/UserData.java
@@ -0,0 +1,105 @@
+package com.restaurantManagement.models;
+
+import com.restaurantManagement.dao.DatabaseConnection;
+import lombok.NonNull;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Random;
+
+public class UserData {
+
+    // make connection to sql
+    Connection connection;
+    //private static int idValue= 10000;
+
+    // list of all users
+    private final List<User> userData = new ArrayList<>();
+
+
+    // constructor --> get all users at first
+    public UserData() {
+        try {
+            connection = DatabaseConnection.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("select * from users");
+            ResultSet resultSet = preparedStatement.getResultSet();
+            while(resultSet.next()) {
+                String fname = resultSet.getString("fname");
+                String lname = resultSet.getString("lname");
+                String password = resultSet.getString("password");
+                String email = resultSet.getString("email");
+                String phone = resultSet.getString("phone");
+                userData.add(new User(fname,lname,password,email,phone));
+            }
+            resultSet.close();
+            preparedStatement.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void addUser(User addUser) throws SQLException {
+        // make unique id for each user
+        Random random = new Random();
+        boolean flag = true;
+        PreparedStatement preparedStatement = connection.prepareStatement("select userId from users");
+        ResultSet resultSet = preparedStatement.getResultSet();
+        int idValue = 10000;
+        while(flag) {
+            idValue = 10000 + random.nextInt(90000);
+            if ((idValue+"") != resultSet.getString("userId")) {
+                flag = false;
+            }
+        }
+        resultSet.close();
+        preparedStatement.close();
+        // creating has been finished here
+        // add user to list
+        addUser.setUserId(idValue+"");
+        userData.add(addUser);
+    }
+
+    public void removeUser(String userId) throws SQLException {
+        ListIterator<User> itemListIterator = userData.listIterator();
+        while (itemListIterator.hasNext()) {
+            User item = itemListIterator.next();
+            if (item.getUserId() == userId) {
+                // delete user in users table
+                Statement statement = connection.createStatement();
+                statement.execute("delete from userId where userId = ''"+ userId + "''");
+                statement.close();
+                itemListIterator.remove();
+                break;
+            }
+        }
+    }
+
+    public User getUser(String userId) {
+        for (User item : userData) {
+            if (item.getUserId() == userId) {
+                return item;
+            }
+        }
+        return null;
+    }
+
+    public void updateUser(@NonNull User updataUser) throws SQLException {
+        ListIterator<User> itemListIterator = userData.listIterator();
+
+        while(itemListIterator.hasNext()) {
+            User item = itemListIterator.next();
+
+            if (item.equals(updataUser)) {
+                // user can update his password
+                String password = updataUser.getPassword();
+                Statement statement = connection.createStatement();
+                statement.execute("update users set password = '" + password + "'");
+                statement.close();
+                itemListIterator.set(updataUser);
+                break;
+            }
+        }
+    }
+
+}
