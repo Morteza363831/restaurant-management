ea42c69f2eb7248a038ab7b652569136ba409971
Morteza363831 <120910275+Morteza363831@users.noreply.github.com>
2024-05-22 19:52:48 +0330

    set dispatcher servlet and its configuration to web app . view resolver has been configured too .

--- src/main/java/com/restaurantManagement/config/WebAppInitializer.java
+++ src/main/java/com/restaurantManagement/config/WebAppInitializer.java
@@ -1,4 +1,24 @@
 package com.restaurantManagement.config;
 
-public class WebAppInitializer {
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+@Slf4j
+public class WebAppInitializer implements WebApplicationInitializer {
+
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        log.info("on startup -----> restaurant management");
+
+        // create spring application context
+        AnnotationConfigWebApplicationContext context =
+                new AnnotationConfigWebApplicationContext();
+        context.setConfigLocation("classpath:app-config.xml");
+
+    }
 }
